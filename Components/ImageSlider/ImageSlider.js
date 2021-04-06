b7db5a409510405ef81e1053e0ea16f48bba71cb
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-06 16:19:35 +0200

    Show previous slide on previous btn click

--- Components/ImageSlider/ImageSlider.js
+++ Components/ImageSlider/ImageSlider.js
@@ -8,15 +8,38 @@ let generateArrowsFor = (container) => {
 
   container.insertBefore(newLeftArrow, null);
   container.appendChild(newRightArrow);
+
+  return [newLeftArrow, newRightArrow];
 };
+
+let showPreviousSlide = (e) => {
+  let slider, slides, currentSlide, indexOfCurrentSlide;
+
+  slider = e.target.parentNode;
+  slides = Array.from(slider.querySelectorAll('.ImageSlider__slide'));
+
+  currentSlide = document.querySelector(
+    '.ImageSlider__slide.ImageSlider__slide--visible'
+  );
+  indexOfCurrentSlide = slides.indexOf(currentSlide);
+
+  let newCurrentIndex;
+  if (indexOfCurrentSlide) {
+    newCurrentIndex = indexOfCurrentSlide - 1;
+  } else {
+    newCurrentIndex = slides.length - 1;
+  }
+
+  currentSlide.classList.remove('ImageSlider__slide--visible');
+  slides[newCurrentIndex].classList.add('ImageSlider__slide--visible');
+};
+
+let showNextSlide = (e) => {};
 
 if (imageSliders) {
   imageSliders.forEach((slider) => {
-    generateArrowsFor(slider);
+    let arrows = generateArrowsFor(slider);
+    arrows[0].addEventListener('click', showPreviousSlide);
+    arrows[1].addEventListener('click', showNextSlide);
   });
 }
-
-document.addEventListener('click', function (e) {
-  if (e.target.parentNode.classList.contains('ImageSlider')) {
-  }
-});
