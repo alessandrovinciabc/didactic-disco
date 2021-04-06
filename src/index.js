8cf88650bd8e2b9f1a0a5bdc493721f109dd4797
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-06 15:41:20 +0200

    Complete styles for image slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import InitializeDropDownMenus from '../Components/DropDownMenu/DropDownMenu.js';
 import InitializeMobileMenus from '../Components/MobileMenu/MobileMenu.js';
+import initializeImageSliders from '../Components/ImageSlider/ImageSlider.js';
 
 InitializeDropDownMenus();
